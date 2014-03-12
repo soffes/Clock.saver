978350fc014d58519fe0f7ac78c7bbf2a023c723
Sam Soffes <sam@soff.es>
2014-03-12 08:58:11 -0700

    Make the test app actually load the bundle

--- ClockTest/Classes/AppDelegate.h
+++ ClockTest/Classes/AppDelegate.h
@@ -6,12 +6,9 @@
 //  Copyright (c) 2014 Sam Soffes. All rights reserved.
 //
 
-@class SAMClockView;
-
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
-@property IBOutlet SAMClockView *clockView;
 
 - (IBAction)showConfiguration:(id)sender;
 
--- ClockTest/Classes/AppDelegate.m
+++ ClockTest/Classes/AppDelegate.m
@@ -9,16 +9,42 @@
 #import "AppDelegate.h"
 #import "SAMClockView.h"
 
+@interface AppDelegate ()
+@property (nonatomic) ScreenSaverView *clockView;
+@end
+
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	[self.clockView startAnimation];
-	[NSTimer scheduledTimerWithTimeInterval:[self.clockView animationTimeInterval] target:self.clockView selector:@selector(animateOneFrame) userInfo:nil repeats:YES];
+
+#pragma mark - Actions
+
+- (IBAction)showConfiguration:(id)sender {
+	[NSApp beginSheet:self.clockView.configureSheet modalForWindow:self.window modalDelegate:self didEndSelector:@selector(endSheet:) contextInfo:nil];
 }
 
 
-- (IBAction)showConfiguration:(id)sender {
-	[NSApp beginSheet:self.clockView.configureSheet modalForWindow:self.window modalDelegate:nil didEndSelector:nil contextInfo:NULL];
+#pragma mark - Private
+
+- (void)endSheet:(NSWindow *)sheet {
+	[sheet close];
+}
+
+
+#pragma mark - NSApplicationDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	NSBundle *clockBundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"Clock" ofType:@"saver"]];
+	[clockBundle load];
+
+	Class principalClass = [clockBundle principalClass];
+
+	self.clockView = [[principalClass alloc] init];
+	self.clockView.frame = [self.window.contentView bounds];
+	self.clockView.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+	[self.window.contentView addSubview:self.clockView];
+
+	[self.clockView startAnimation];
+	[NSTimer scheduledTimerWithTimeInterval:[self.clockView animationTimeInterval] target:self.clockView selector:@selector(animateOneFrame) userInfo:nil repeats:YES];
 }
 
 @end
