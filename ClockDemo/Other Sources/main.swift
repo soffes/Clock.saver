18d81d31f7eaab82ded3802bd83b2215a4fd38bd
Sam Soffes <sam@soff.es>
2014-07-16 23:31:23 -0700

    Remove old stuff needed for Objective-C

---
+++ ClockDemo/Other Sources/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Clock
+//
+//  Created by Sam Soffes on 7/16/14.
+//  Copyright (c) 2014 Sam Soffes. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
